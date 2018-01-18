34349a5eaea864daf3d7c7de34bca03caf712df0
Volski <volski.alex@gmail.com>
2018-01-18 11:46:35 +0200

    Initial Commit

---
+++ testgit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  testgit
+//
+//  Created by MyMac on 18/01/2018.
+//  Copyright © 2018 MyMac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
